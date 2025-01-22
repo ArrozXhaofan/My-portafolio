77f86ee8bede08aee9b4b6af20920b5a4be4cf58
Jeanpiere Laura <jean.ganador12@gmail.com>
2025-01-22 00:03:45 -0500

    v4

--- app/experiencia/page.tsx
+++ app/experiencia/page.tsx
@@ -1,6 +1,5 @@
 "use client"
 
-import React, { useState } from 'react'
 import Image from 'next/image'
 import Navigatore, { SubNavConfiguration } from '../Components/Nav/Nav'
 import ExpCard, { ExpModel } from '../Components/ExpCard/ExpCard'
